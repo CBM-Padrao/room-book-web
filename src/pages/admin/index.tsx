c509ad5d1e4911e36f07ec7f70c55e92d7dcd903
FabszZ20 <fabriciokurama@gmail.com>
2024-11-10 20:51:16 -0300

    refactor: Trocar nome de componente

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,7 +1,7 @@
-import { Table } from '../../components/Table';
+import { UserTable } from '../../components/UserTable';
 
 export function Admin() {
   // Redirect to the admin login page if the user is not authenticated and it's not an admin
 
-  return <Table />;
+  return <UserTable />;
 }
