97c0424dd09901ad620cb568e0ca5ba49e7969e2
Gabriel Rasteli <gabrielrasteli3@gmail.com>
2024-11-16 12:31:30 -0300

    refactor: Remover comentário

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,7 +1,5 @@
 import { UserTable } from '../../components/UserTable';
 
 export function Admin() {
-  // Redirect to the admin login page if the user is not authenticated and it's not an admin
-
   return <UserTable />;
 }
