d2242af64dec0fe348cc8ebf5ef634db9ac48e04
FabszZ20 <fabriciokurama@gmail.com>
2024-11-16 21:03:16 -0300

    feat: editar usuário logado

--- src/pages/account.tsx
+++ src/pages/account.tsx
@@ -1,10 +1,13 @@
-import { useState } from 'react';
-import { Button, Input, InputGroup } from 'rsuite';
+import { FormEvent, useState } from 'react';
+import { Button, Input, InputGroup, Notification, useToaster } from 'rsuite';
 import { EyeClose, EyeRound } from '@rsuite/icons';
 import { useAuth } from '../contexts/AuthContext';
+import { useUser } from '../contexts/UserContext';
 
 export function Account() {
   const { user, signOut } = useAuth();
+  const { updateUser } = useUser();
+  const toaster = useToaster();
 
   const userName = user!.name.split(' ');
   const lastNames = userName.slice(1).join(' ');
@@ -16,13 +19,46 @@ export function Account() {
 
   const [passwordVisible, setPasswordVisible] = useState(false);
 
+  async function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+
+    const newUser = {
+      name: `${name} ${surname}`,
+      email,
+      password,
+      registration: user!.registration,
+      role: user!.role
+    };
+
+    try {
+      await updateUser({ ...user!, password }, newUser);
+      setPassword('');
+
+      toaster.push(
+        <Notification type="success" header="Operação sucedida" closable>
+          O usuário foi atualizado com sucesso.
+        </Notification>,
+        { placement: 'bottomEnd', duration: 5000 }
+      );
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (error: any) {
+      toaster.push(
+        <Notification type="error" header="Erro ao atualizar usuário" closable>
+          {error.response?.data.message ||
+            'Houve um erro ao atualizar o usuário. Tente novamente.'}
+        </Notification>,
+        { placement: 'bottomEnd', duration: 5000 }
+      );
+    }
+  }
+
   return (
     <div className="mx-auto w-[980px] gap-10 p-20 flex flex-col">
       <h1 className="text-3xl">Olá, {user!.name}</h1>
 
       <div className="border-b-[1px] border-[#292d33]"></div>
 
-      <form className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         Matrícula
         <Input disabled value={user!.registration} />
         <div className="flex justify-between gap-4">
@@ -41,6 +77,7 @@ export function Account() {
         <InputGroup inside>
           <Input
             type={passwordVisible ? 'text' : 'password'}
+            value={password}
             onChange={setPassword}
           />
           <InputGroup.Button
